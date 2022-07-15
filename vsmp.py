ec29eedabe2e2828e7fee70399260dc7fa56f0cc
bredmondG <bredmondgold@gmail.com>
2022-07-15 16:08:46 -0600

    updated for v2

--- vsmp.py
+++ vsmp.py
@@ -4,7 +4,8 @@ import sys
 import os
 import ffmpeg
 import logging
-import epd7in5
+import epd7in5_V2
+import epd7in5bc
 import time
 from threading import Thread
 from PIL import Image,ImageDraw,ImageFont
@@ -17,45 +18,50 @@ import random
 logging.basicConfig(filename='log.txt', filemode='w', level=logging.DEBUG)
 logging.warning('hello log')
 
-def slice_video():
-    #cuts yojimbo into 20 equal sections
-    video = 'YOJIMBO.m4v'
-    s = os.popen('ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 {}'.format(video))
+def slice_video(filename, start_range = 0):
+    #cuts file into 20 equal sections
+    stop_range = start_range + 20
+    movie_name = filename.split(".")[0]
+    file_type = "." + filename.split(".")[1]
+    s = os.popen('ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 {}'.format(filename))
     seconds = float(s.read().split()[0])
     duration = seconds/20
     t = 0
-    for i, section in enumerate(range(0,10)):
+    for i in range(start_range, stop_range):
         print(t, t + duration)
-        command = 'ffmpeg -i YOJIMBO.m4v -ss {} -t {} -c copy yojimbo/yojimbo_section{}.m4v'.format(t, duration, i)
-        os.popen(command)
+        command = 'ffmpeg -i {} -ss {} -t {} -c copy {}/{}_section{}{}'.format(filename, t, duration, movie_name, movie_name, i, file_type)
+        output = os.popen(command)
+        print(output.read())
         t+= duration
         
-def frame_count(clip):
-    fast_count = 'ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 yojimbo/{}'.format(clip)
+def frame_count(clip, movie_name):
+    fast_count = 'ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=nokey=1:noprint_wrappers=1 {}/{}'.format(movie_name, clip)
     s = os.popen(
                 fast_count
                 ).read()
     frames = int(s.split()[0])
     return frames
         
-def generate_frames(clip, frame, frame_len):
+def generate_frames(clip, frame, frame_len, movie_name):
+    folder = '{}_frames'.format(movie_name)
     
     while frame < frame_len:
-        if len(os.listdir('yojimbo_frames')) < 3000:
-            if ('out_img%d.jpg' %(frame)) not in os.listdir('yojimbo_frames'):
-                os.system('ffmpeg -i yojimbo/{} -vf "select=gte(n\,{})" -vframes 1 yojimbo_frames/out_img{}.jpg'.format(clip,frame, frame))
+        if len(os.listdir(folder)) < 3000:
+            if ('out_img%d.jpg' %(frame)) not in os.listdir(folder):
+                os.system('ffmpeg -i {}/{} -vf "select=gte(n\,{})" -vframes 1 {}/out_img{}.jpg'.format(movie_name, clip, frame, folder, frame))
             frame +=1
 
     logging.info("generate_frames done")
     
-def generate_frame(clip, frame):
-    os.popen('ffmpeg -i yojimbo/{} -vf "select=gte(n\,{})" -vframes 1 yojimbo_frames/out_img{}.jpg'.format(clip,frame, frame)).read()
+def generate_frame(clip, frame, movie_name):
+    folder = '{}_frames'.format(movie_name)
+    os.popen('ffmpeg -i {}/{} -vf "select=gte(n\,{})" -vframes 1 {}/out_img{}.jpg'.format(movie_name, clip,frame, folder, frame)).read()
     logging.info("generated_frame: {}".format(frame))
         
         
 def display_frame(clip, frame, frame_len, progress):
     try:        
-        epd = epd7in5.EPD()
+        epd = epd7in5_V2.EPD()
         logging.info("init and Clear")
         epd.init()
         epd.Clear()
@@ -67,22 +73,23 @@ def display_frame(clip, frame, frame_len, progress):
         
     except KeyboardInterrupt:    
         logging.info("ctrl + c:")
-        epd7in5bc.epdconfig.module_exit()
+        epd7in5_V2.epdconfig.module_exit()
         exit()
 
 
-def display_frame(clip, frame, frame_len, progress, epd):
+def display_frame(clip, frame, frame_len, progress, epd, movie_name):
+    folder = '{}_frames'.format(movie_name)
     while frame < frame_len:
         start_t = time.time()
         logging.info("section: {}".format(clip))
         logging.info("frames in section: %d" %frame_len)
         logging.info("Frame: {}".format(frame))
-        frame_path = 'yojimbo_frames/out_img%d.jpg' % (frame)
+        frame_path = '%s/out_img%d.jpg' % (folder, frame)
         # if frame_path doesn't exist or doesn't contain anything
         if (not Path(frame_path).exists()) or (os.stat(frame_path).st_size == 0):
-            generate_frame(clip,frame)
-        im = Image.open(os.path.join('yojimbo_frames/out_img%d.jpg' % (frame)))
-        sized = im.resize((640,384), Image.ANTIALIAS)
+            generate_frame(clip,frame, movie_name)
+        im = Image.open(os.path.join('%s/out_img%d.jpg' % (folder, frame)))
+        sized = im.resize((800,480), Image.ANTIALIAS)
         epd.display(epd.getbuffer(sized))
         os.remove(frame_path)
         frame += 1
@@ -108,36 +115,41 @@ def load_data(file, data):
             return pickle.load(f)
     return data
 
-def play_random_movie(epd):
+def play_random_movie(epd, filename):
+    movie_name = filename.split(".")[0]
+    file_type = "." + filename.split(".")[1]
+    folder = '{}_frames'.format(movie_name)
     progress = load_data('progress.pkl', {
-                        'sections' : os.listdir('yojimbo'),
+                        'sections' : os.listdir(movie_name),
                         'sections_ran': [],
                         'frame' : 0
                         }
                         )
     while True:
         i = random.randint(0, len(progress['sections'])-1)
-        clip = 'yojimbo_section{}.m4v'.format(i)
-        frame = random.randint(0, frame_count(clip) - 5)
+        clip = '{}_section{}{}'.format(movie_name, i, file_type)
+        frame = random.randint(0, frame_count(clip, movie_name) - 5)
         frame_len = frame + 1
         logging.info("Section: {}".format(clip))
-        display_frame(clip, frame, frame_len, progress,epd)
+        display_frame(clip, frame, frame_len, progress,epd, movie_name)
 
-def play_movie(epd):
+def play_movie(epd, filename):
+    movie_name = filename.split(".")[0]
+    file_type = "." + filename.split(".")[1]
     progress = load_data('progress.pkl', {
-                            'sections' : os.listdir('yojimbo'),
+                            'sections' : os.listdir(movie_name),
                             'sections_ran': [],
                             'frame' : 0
                             }
                          )
     logging.info("Playing Movie at frame: {}".format(progress['frame']))
     for i in range(0, len(progress['sections'])):
-        clip = 'yojimbo_section{}.m4v'.format(i)
+        clip = '{}_section{}{}'.format(movie_name, i, file_type)
         if clip not in progress['sections_ran']:
             frame = progress['frame']
-            frame_len = frame_count(clip)- 5
+            frame_len = frame_count(clip, movie_name)- 5
             logging.info("Section: {}".format(clip))
-            display_frame(clip, frame, frame_len, progress, epd)
+            display_frame(clip, frame, frame_len, progress, epd, movie_name)
             progress['sections_ran'].append(clip)
             progress['frame'] = 0
             save_data('progress.pkl', progress)
@@ -148,11 +160,11 @@ def play_movie(epd):
 
 if __name__ == '__main__':
     try:        
-        epd = epd7in5.EPD()
+        epd = epd7in5_V2.EPD()
         logging.info("init and Clear")
         epd.init()
         epd.Clear()
-        play_random_movie(epd)
+        play_movie(epd, "2001.mp4")
 
         
     except IOError as e:
@@ -160,7 +172,7 @@ if __name__ == '__main__':
         
     except KeyboardInterrupt:    
         logging.info("ctrl + c:")
-        epd7in5bc.epdconfig.module_exit()
+        epd7in5_V2.epdconfig.module_exit()
         exit()
 
 
