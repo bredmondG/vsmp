e9fd0fe875294d320a889b341ddcdab4f9014101
bredmondG <bredmondgold@gmail.com>
2023-09-09 14:09:19 -0600

    added server folder

---
+++ server/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+print("hello")
+
+app = Flask(__name__)
+
+def display_logs():
+    with open('../log.txt', 'r') as log_file:
+        logs = log_file.readlines()
+    return logs[-10:]
+
+@app.route('/')
+def home():
+    frame = "current_frame.jpg"
+    return render_template('logs.html', logs = display_logs(), image_filename=frame)
+
+@app.route('/frame')
+def display_frame():
+    frame = "current_frame.jpg"
+    return render_template('frame.html', image_filename=frame)
+
+
+def hello_world():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
